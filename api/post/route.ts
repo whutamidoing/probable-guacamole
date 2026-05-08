d21e050c848f9af5f6e5baf0c157823187f5d42e
whutamidoing <negispringfield23@gmail.com>
2026-05-08 10:30:19 +0800

    post api adapter

--- api/post/route.ts
+++ api/post/route.ts
@@ -12,16 +12,7 @@ export const prisma = new PrismaClient({
 */
 
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
   try {
