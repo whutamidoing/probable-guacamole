c05686ea896241b2a0c25d5774d4a72b418775b4
whutamidoing <negispringfield23@gmail.com>
2026-05-09 21:31:25 +0800

    user route id GET method

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(req: Request) {
-  const { id } = await req.json();
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = params;
 
   const user = await prisma.user.findUnique({
     where: { id },
