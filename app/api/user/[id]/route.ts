a9b4a4d4b5e8fd2df53e24a3573ee4f9279cfdfa
whutamidoing <negispringfield23@gmail.com>
2026-05-12 09:14:21 +0800

    user PATCH method #1

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -17,3 +17,38 @@ export async function GET(
 
   return NextResponse.json(user);
 }
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const body = await req.json();
+    console.log("Body received:", body);
+    let { fName, lName, profileImg, bannerImg } = body;
+
+    if (!fName && !lName && !profileImg && !bannerImg) {
+      console.error("No change to add");
+      return NextResponse.json({ error: "No change to add" }, { status: 400 });
+    }
+
+    const { id } = await params;
+
+    const user = await prisma.user.update({
+      where: { id },
+      data: { fName, lName, profileImg, bannerImg },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error("POST /api/user error:", error);
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 },
+    );
+  }
+}
