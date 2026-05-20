60b9240f7cf0929cbf6828800dd1ce657a3ee551
whutamidoing <negispringfield23@gmail.com>
2026-05-20 16:18:20 +0800

    reuse pool connections

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,12 +2,20 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
-const prisma = new PrismaClient({
-  adapter,
-});
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    }),
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
