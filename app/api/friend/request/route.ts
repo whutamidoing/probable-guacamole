84076a80cbd4ef80b351effae56ef689e0ce1dda
whutamidoing <negispringfield23@gmail.com>
2026-05-25 14:34:58 +0800

    request params

--- app/api/friend/request/route.ts
+++ app/api/friend/request/route.ts
@@ -1,11 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: Promise<{ id: string }> },
-) {
+export async function GET() {
   try {
     const { userId } = await auth();
 
