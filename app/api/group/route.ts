3071b7a76a03935e80ec9a6e4b5b5e91b23395c2
whutamidoing <negispringfield23@gmail.com>
2026-06-24 14:29:23 +0800

    allow all test

--- app/api/group/route.ts
+++ app/api/group/route.ts
@@ -3,7 +3,7 @@ import { auth } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/db";
 import { Group } from "@prisma/client";
 
-const ALLOWED_ORIGIN = "http://localhost:3000";
+const ALLOWED_ORIGIN = "*";
 
 export async function POST(req: NextRequest) {
   try {
