683d12315f3b99ffd4265bf176fe2fdb202f5687
whutamidoing <negispringfield23@gmail.com>
2026-07-06 23:15:33 +0800

    exclude and nest replies

--- app/api/comment/route.ts
+++ app/api/comment/route.ts
@@ -80,7 +80,7 @@ export async function GET(req: NextRequest) {
   const postId = Number(searchParams.get("postId"));
 
   const comments = await prisma.comment.findMany({
-    where: { postId: postId || undefined },
+    where: { postId: postId || undefined, parentId: null },
     include: {
       commenter: true,
       replies: {
